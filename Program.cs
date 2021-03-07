de9601dc29a69e78ee1f3fac7cb8424a3d097a0f
punisher96 <65577214+punisher96@users.noreply.github.com>
2021-03-07 12:31:46 -0400

    Cambio del hola mundo
    cambiamos la frases

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tarea6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Amadis esta es nuestra presentacion de github");
+        }
+    }
+    }
