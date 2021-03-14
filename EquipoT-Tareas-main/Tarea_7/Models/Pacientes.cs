b0805519b08cfce86caa84ca04f381fd2b48d601
Raphiel28 <raphielburdier72@gmail.com>
2021-03-13 21:14:31 -0500

    Ejercicios 3 y 4

---
+++ EquipoT-Tareas-main/Tarea_7/Models/Pacientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Tarea_7.Models
+{
+    public partial class Pacientes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string cedula { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public int? VacunaId { get; set; }
+        public int? ProvinciaId { get; set; }
+        public int? SignoId { get; set; }
+        public DateTime? FechaPrimeraVacuna { get; set; }
+        public DateTime? FechaSegundaVacuna { get; set; }
+
+        public virtual Provincias Provincia { get; set; }
+        public virtual Signozodiacal Signo { get; set; }
+        public virtual Vacunas Vacuna { get; set; }
+    }
+}
---
+++ EquipoT-Tareas-main/Tarea_7/Models/Vacunas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Tarea_7.Models
+{
+    public partial class Vacunas
+    {
+        public Vacunas()
+        {
+            Pacientes = new HashSet<Pacientes>();
+        }
+
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public int? Cantidad { get; set; }
+
+        public virtual ICollection<Pacientes> Pacientes { get; set; }
+    }
+}
